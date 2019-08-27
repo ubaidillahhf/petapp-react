f97d18524c8cc01f5be043ecf3d7b0027d3e151b
ubaidillahhf <ubaidillahhf@gmail.com>
2019-08-27 13:52:34 +0700

    add useState hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+// import Coba from "./Coba";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt me"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Papper",
-  //     animal: "Bird",
-  //     breed: "Cockatail"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed"
-  //   })
-  // ]);
   return (
     <div>
       <h1>adopt-me</h1>
-      <Pet name="luna" animal="Dog" breed="havanese" />
-      <Pet name="papper" animal="Bird" breed="cocktail" />
-      <Pet name="doink" animal="Cat" breed="mixed" />
+      <SearchParams />
+      {/* <Coba /> */}
     </div>
   );
 };
---
+++ src/Coba.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// class Coba extends React.Component() {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello World</h1>
+//       </div>
+//     );
+//   }
+// }
+
+export default class Coba extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
